f5eaa6296d714944a3efeffe3266e76ce1070319
Haneul0819 <s2018s08@e-mirim.hs.kr>
2020-06-04 16:22:55 +0900

    override method 예제

--- overridemethos/Program.cs
+++ overridemethos/Program.cs
@@ -8,6 +8,39 @@ namespace overridemethos
 {
     class Program
     {
+        int Power(int input)
+        {
+            return input * input;
+        }
+
+        int Power(int input, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                input *= input;
+            }
+            return input;
+        }
+
+        int SumAll(int end)
+        {
+            int sum = 0;
+            for (int i = 0; i < end; i++)
+            {
+                sum += i;
+            }
+            return end;
+        }
+
+        int SumAll(int start, int end)
+        {
+            int sum = 0;
+            for (int i = start; i < end; i++)
+            {
+                sum += start;
+            }
+            return sum;
+        }
         static void Main(string[] args)
         {
         }
